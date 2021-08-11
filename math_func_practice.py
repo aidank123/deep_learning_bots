2edc5c47c7a8945e18a8de6007dc164abed5125a
aidank123 <askelley@smcm.edu>
2021-08-11 14:01:05 +0200

    numpy impertinated

--- math_func_practice.py
+++ math_func_practice.py
@@ -10,10 +10,8 @@ def elementwise_multiplication(vec_a, vec_b):
         assert(len(vec_a) == len(vec_b))    
         output = []
         i = 0
-        
         for i in range (len(vec_a)):
             output.append(vec_a[i] * vec_b[i])
-        
         return output
     except AssertionError:
             print("Those lists are not the same length!")
@@ -24,10 +22,8 @@ def elementwise_addition(vec_a, vec_b):
         assert(len(vec_a) == len(vec_b))    
         output = []
         i = 0
-        
         for i in range (len(vec_a)):
             output.append(vec_a[i] + vec_b[i])
-        
         return output
     except AssertionError:
             print("Those lists are not the same length!")
@@ -35,26 +31,21 @@ def elementwise_addition(vec_a, vec_b):
 def vector_sum(vec_a):
     
     total = 0
-    
     for elements in vec_a:
-        
         total = total + elements
     return total
 
 def vector_average(vec_a):
     
     total = 0
-    
     for elements in vec_a:
-        
         total = total + elements
-        
     return total/len(vec_a)
 
 def weighted_sum(a,b):
     
     c = elementwise_multiplication(a,b)
-    
     return vector_sum(c)
+
 
 # print(vector_average(vec_a))
