f614aba4ae97b05c6ad982468501d39b646dc5ba
aidank123 <askelley@smcm.edu>
2021-08-23 14:20:49 -0400

    chap 3 finish

--- main.py
+++ main.py
@@ -2,13 +2,31 @@
 import numpy as np
 import math_func_practice as ma
 
-weights = np.array([0.1,0.2,0])
+# toes % wins # fans
+
+
+# weights = np.array([0.1,0.2,0])
+ih_wgt = np.array([
+                  [0.1, 0.2, -0.1], #hid[0]
+                  [-0.1, 0.2, 0.9], #hid[1]
+                  [0.0, 0.4, 0.1]]).T #hid[2]
+
+# hid[0], hid[1], hid[2]
+
+hp_wgt = np.array([
+                  [0.3, 1.1, -0.3], # hurt?
+                  [0.1, 0.2, 0.0],  #win?
+                  [0.1, 0.4, 0.1]]).T #sad?
+
+weights = [ih_wgt, hp_wgt]
 
 def neural_network(input, weights):
     
 #     prediction = ma.weighted_sum(input,weights)
-    prediction = input.dot(weights)
-    return prediction
+    hid = input.dot(weights[0])
+    pred = input.dot(weights[1])
+    #prediction = input.dot(weights)
+    return pred
 
 
 toes = np.array([8.5, 9.5, 10, 9])
