58c09d5b213116cd67b9af2cb2a134d09aa7b4f5
aidank123 <askelley@smcm.edu>
2021-08-29 00:53:17 -0400

    workin thru chapter 4, can predict goal number using 'hot and cold'

---
+++ chap_4.py
@@ -0,0 +1,35 @@
+#Chapter 4 main
+
+# knob_weight = 0.5
+# input = 0.5
+# goal_pred = 0.8
+# 
+# pred = input * knob_weight
+# 
+# error = (pred - goal_pred) ** 2
+# print(error)
+
+weight = 0.5
+input = 0.5
+goal_prediction = 0.8
+step_amount = 0.001
+
+for iteration in range(1101):
+    
+    prediction = input * weight
+    error = (prediction - goal_prediction) ** 2
+    
+    print("Error:" + str(error) + " Prediction:" + str(prediction))
+    
+    up_prediction = input * (weight + step_amount)
+    up_error = (goal_prediction - up_prediction) ** 2
+    
+    down_prediction = input * (weight - step_amount)
+    down_error = (goal_prediction - down_prediction) ** 2
+    
+    if(down_error < up_error):
+        weight = weight - step_amount
+        
+    elif(down_error > up_error):
+        weight = weight + step_amount
+        
